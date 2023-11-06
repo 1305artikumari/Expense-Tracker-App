e44a98f1b2e1958146dee5aa1e5ccf0c59730f60
Arti Kumari <artikumari09011999@gmail.com>
2023-11-06 19:37:46 +0530

    Daily Expense App features

--- app.js
+++ app.js
@@ -2,36 +2,31 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const sequelize = require('./config/db');
+const Expenses = require('./models/expenses');
 const User = require('./models/users');
 const mainPageRouter = require('./routes/mainpage');
 const userRouter = require('./routes/user');
-// const PORT = process.env.PORT;
-// const PORT = process.env.PORT || 4000; // Use the provided PORT environment variable or a default value (e.g., 3000)
+const expenseRouter = require('./routes/expenses');
+
 
 const port = 7000;
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+User.hasMany(Expenses);
+Expenses.belongsTo(User,{ constraints: true,
+onDelete: 'CASCADE'});
+
 app.use(mainPageRouter)
 app.use('/user', userRouter);
+app.use('/expenses',expenseRouter);
 
-// async function initiate() {
-//   try {
-//     await sequelize.sync();
-//     app.listen(PORT, () => {
-//       console.log(`Server is running at ${PORT}`);
-//     });
-//   }
-//     catch (error) {
-//       console.log(error);
-    
-//   }
-// }
-// initiate();
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- controllers/user.js
+++ controllers/user.js
@@ -3,25 +3,20 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY;
 exports.usergethomePage = (request, response, next) => {
-    
+
     response.sendFile('main.html', { root: 'views' });
-   
+
 }
 exports.signupAuthentication = async (request, response, next) => {
     const { Name, userName, password } = request.body;
     try {
         const user = await Userservices.getUserbyemail(userName);
-        
         if (!user) {
-           
             const hashedPassword = await bcrypt.hash(password, 10);
-            
             const user = await Userservices.createUser(Name, userName, hashedPassword);
             const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
-           
             response.status(200).send({ token: token, user: user });
         } else {
-            
             response.status(401).send(user);
         }
 
@@ -31,21 +26,21 @@ exports.signupAuthentication = async (request, response, next) => {
 }
 exports.signinAuthentication = async (request, response, next) => {
     try {
-       
+
         const { userName, password } = request.body;
         const user = await Userservices.getUserbyemail(userName);
-        
+
         if (!user) {
-            
+
             response.status(404).send('User not found');
         } else {
-            
+
             const isPasswordValid = await bcrypt.compare(password, user.password);
-            
+
             if (isPasswordValid) {
-                
-                const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' }); 
-                
+
+                const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+
                 response.status(200).json({ token: token, user: user });
             } else {
                 response.status(401).send('Authentication failed');
