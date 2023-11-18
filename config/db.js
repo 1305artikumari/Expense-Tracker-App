9f46d5af93e97571f3e6800971e4fb3ff7aa9221
Arti Kumari <artikumari09011999@gmail.com>
2023-11-18 18:33:42 +0530

    connection file update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const mysql= new Sequelize('expense', 'root', 'Arti@123', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    logging: false
+  }
+);
 
-module.exports = mysql;
-
-
+module.exports = sequelize;
+module.exports = sequelize;
 
